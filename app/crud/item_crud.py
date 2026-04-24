fec2556655ee702a623c02c4df4a1d78a15ac0bd
Zeratulez <aks.aks.25@mail.ru>
2026-04-24 03:48:57 +0300

    created crud files for user and item models

---
+++ app/crud/item_crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.item import Item
+from app.schemas import item_schema, user_schema
+
+async def get_item_by_id(session: AsyncSession, item_id: int):
+    return await session.get(Item, item_id)
+
+async def get_items(session: AsyncSession, name: str, skip: int, limit: int):
+    query = select(Item).filter(Item.name.contains(name)).offset(skip).limit(limit)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+async def create_item(session: AsyncSession, user: user_schema.UserInDB, item: item_schema.ItemCreate):
+    new_item = Item(**item.model_dump(), owner_id=user.id)
+    session.add(new_item)
+    await session.commit()
+    await session.refresh(new_item)
+    return new_item
+
+async def update_item(session: AsyncSession, item_db: Item, new_item_data: item_schema.ItemUpdate):
+    for key, value in new_item_data.model_dump(exclude_unset=True).items():
+        setattr(item_db, key, value)
+    await session.commit()
+    await session.refresh(item_db)
+    return item_db
+
+async def delete_item(session: AsyncSession, item: Item):
+    await session.delete(item)
+    await session.commit()
+    return {"message": "Item successfully deleted"}
---
+++ app/crud/user_crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import EmailStr
+
+from app.models.user import User
+from app.schemas import user_schema
+
+async def get_user_by_id(session: AsyncSession, user_id: int):
+    return await session.get(User, user_id)
+
+async def get_user_by_username(session: AsyncSession, username: str):
+    query = select(User).filter(User.username == username)
+    return await session.scalar(query)
+
+async def create_user(session: AsyncSession, user: user_schema.UserCreate, hashed_password: str):
+    new_user = User(**user.model_dump(exclude={"password"}), hashed_password=hashed_password)
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
+
+async def change_password(session: AsyncSession, user: User, hashed_password: str):
+    user.hashed_password = hashed_password
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return {"message": "Password changed successfully"}
+
+async def get_user_by_email(session: AsyncSession, email: EmailStr):
+    query = select(User).filter(User.email == email)
+    return await session.scalar(query)
