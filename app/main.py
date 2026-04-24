4c79ce32d0b76ab559e17a4e0ea268849e959669
Zeratulez <aks.aks.25@mail.ru>
2026-04-24 20:59:53 +0300

    added router to main.py and added files .gitignore & alembic.ini

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from app.api.endpoints import auth, items
+
 app = FastAPI()
 
-app.include_router()
+app.include_router(auth.router)
+app.include_router(items.router)
